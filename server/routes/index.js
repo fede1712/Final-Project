8c31221d60cfc832d111558463dea53252eec9ea
MATS <waanmedia_mats@MacBook-Pro-de-WaanMedia-MATS.local>
2021-09-25 12:17:44 +0200

    day1

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,7 @@
 const router = require("express").Router();
-
 router.get("/", (req, res, next) => {
-  res.json("All good in here");
+    res.json("All good in here");
 });
-
-// You put the next routes here 👇
+// You put the next routes here :apuntando_hacia_abajo:
 // example: router.use("/auth", authRoutes)
-
 module.exports = router;
