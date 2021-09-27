c9caee73ba9f16b4d8043ef152ed131c752c43f6
Sergio Puente <sergio.leones@hotmail.com>
2021-09-27 11:51:52 +0200

    Day 2

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
 const bikesRouter = require('./bikes.routes')
-const shopsRouter = require('./shops.routes')
+// const shopsRouter = require('./shops.routes')
 const authRouter = require('./auth.routes')
 
 
 router.use("/bikes", bikesRouter)
-router.use("/shop", shopsRouter)
+// router.use("/shop", shopsRouter)
 router.use("/auth", authRouter)
 
 
