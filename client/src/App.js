4a8addc69b2991543a6908d48a732bf78a97f0ec
MATS <waanmedia_mats@MacBook-Pro-de-WaanMedia-MATS.local>
2021-09-28 10:45:53 +0200

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
-//import 'bootstrap/dist/css/bootstrap.min.css';
-//import Navigation from './components/layout/Navigation/Navigation';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navigation from './components/layout/Navigation/Navigation';
+import Home from './components/Pages/Home/Home';
 
 function App() {
+
   return (
     <div className="App">
+
       <header className="App-header">
+
+        <div>
+
+          <Navigation />
+
+          < Home />
+
+        </div>
 
       </header>
     </div>
