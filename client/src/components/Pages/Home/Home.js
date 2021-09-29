a28f2f1d4e33c97fef5b4d4f689e84a497368f46
MATS <waanmedia_mats@MacBook-Pro-de-WaanMedia-MATS.local>
2021-09-29 10:58:58 +0200

    done

--- client/src/components/Pages/Home/Home.js
+++ client/src/components/Pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Row, Col } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import "./Home.css"
+import Carousel from 'react-bootstrap/Carousel'
 
 export default function Home(props) {
 
@@ -27,9 +28,10 @@ export default function Home(props) {
                         <Col md={12}>
 
                             <div class="buyNow">
-                                <a href="#" class="">
-                                    Compra ya tu Tricycle 4<i class="fa fa-arrow-right hvr-icon"></i>
-                                </a>
+                                <Link className="buyButtomLink" to="/">Compra ya tu Tricycle 4</Link>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="white" class="bi bi-arrow-right arrow" viewBox="0 0 16 16" >
+                                    <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
+                                </svg>
                             </div>
                         </Col>
                     </Row>
@@ -38,12 +40,36 @@ export default function Home(props) {
 
             </section >
 
-            <section class="prueba">
+            <section className="heroImg2">
+
                 <div >
 
-                </div>
-            </section>
+                    <Row className="alingItems">
+                        <Col md={{ span: 4, offset: 4 }}>
+                            <div class="h1Css">
+                                <h1>Tricycle 4 RS</h1>
+                                <h3>Un marco abierto para una mente abierta.</h3>
 
+                            </div>
+                            <div className="d-flex justify-content-center">
+                                <Link className="btn btn-secondary buttonHero2" variant="outline-secondary" to="/">Más información</Link>
+                            </div>
+
+                        </Col>
+                        <Col md={12}>
+
+                            <div class="buyNow">
+                                <Link className="buyButtomLink" to="/">Compra ya tu Tricycle 4 RS</Link>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="white" class="bi bi-arrow-right arrow" viewBox="0 0 16 16" >
+                                    <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
+                                </svg>
+                            </div>
+                        </Col>
+                    </Row>
+
+                </div>
+
+            </section >
 
         </div >
 
--- client/src/components/layout/Footer/Footer.js
+++ client/src/components/layout/Footer/Footer.js
@@ -1,10 +1,89 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Container, Nav, Navbar, Row, Col, NavDropdown } from 'react-bootstrap'
+import Logo from './logo_tricycle.svg'
+import './Footer.css'
+
 
 export default function Footer() {
     return (
-        <div>
+
+        <section >
+
+            <div class="footerCss">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="row">
+                                <div class="col-md-6 ">
+                                    <div class="logo-part">
+                                        <img src={Logo} class="w-50 logo-footer logoStyle" />
+                                        <p>7637 Laurel Dr.King Of Prussia, PA 19406</p>
+                                        <p>Use this tool as test data for an automated system or find your next pen</p>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-6 px-4">
+                                    <h6> About Company</h6>
+                                    <p>But horizontal lines can only be a full pixel high.</p>
+                                    <a href="#" class="btn-footer"> More Info </a>
+                                    <a href="#" class="btn-footer"> Contact Us</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="row">
+                                <div class="col-md-6 px-4">
+                                    <h6> Help us</h6>
+                                    <div class="row ">
+                                        <div class="col-md-6">
+                                            <ul>
+                                                <li> <a href="#"> Home</a> </li>
+                                                <li> <a href="#"> About</a> </li>
+                                                <li> <a href="#"> Service</a> </li>
+                                                <li> <a href="#"> Team</a> </li>
+                                                <li> <a href="#"> Help</a> </li>
+                                                <li> <a href="#"> Contact</a> </li>
+                                            </ul>
+                                        </div>
+                                        <div class="col-md-6 px-4">
+                                            <ul>
+                                                <li> <a href="#"> Cab Faciliy</a> </li>
+                                                <li> <a href="#"> Fax</a> </li>
+                                                <li> <a href="#"> Terms</a> </li>
+                                                <li> <a href="#"> Policy</a> </li>
+                                                <li> <a href="#"> Refunds</a> </li>
+                                                <li> <a href="#"> Paypal</a> </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6 ">
+                                    <h6> Newsletter</h6>
+                                    <div class="social">
+                                        <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                                        <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                                    </div>
+                                    <form class="form-footer my-3">
+                                        <input type="text" placeholder="search here...." name="search" />
+                                        <input type="button" value="Go" />
+                                    </form>
+                                    <p>That's technology limitation of LCD monitors</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </section >
+
+
+    )
+}
+
+
+/* <div>
             <Container>
                 <Row>
                     <Col>
@@ -15,6 +94,4 @@ export default function Footer() {
                     </Col>
                 </Row>
             </Container>
-        </div>
-    )
-}
+        </div> */
