53d3f5c0366ca262fa53648282b6454dc8c29325
fede marinas <fedemarinasruiz@gmail.com>
2021-10-07 10:40:12 +0200

    deploy

--- server/app.js
+++ server/app.js
@@ -12,12 +12,12 @@ const app = express();
 require("./config")(app);
 require("./config/session.config")(app)
 require("./config/cors.config")(app);
+app.use(express.static(path.join(__dirname, '..', "public")))
 
 const allRoutes = require("./routes");
 app.use("/api", allRoutes);
 
 
-app.use(express.static(path.join(__dirname, '..', "public")))
 app.use((req, res) => res.sendFile(__dirname + "/public/index.html"));
 
 
